5c54b3089e980df65a70f256c79b6ff3a42cec9b
Noureddine Azhar <nrdnazhr@gmail.com>
2020-04-21 17:46:46 +1000

    Added most styles, updated logo, created templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
 //
 
+const aboutOverlay      = document.querySelector('section.about');
+const aboutOpen         = document.querySelector('a.aboutUs');
+const aboutClose        = document.querySelector('a.closeAboutUs');
+
 //
 
-console.log('A starting point.');
+// Toggle about us section
+
+aboutOpen.addEventListener('click', () => {
+
+    aboutOverlay.style.display = 'block';
+
+});
+
+aboutClose.addEventListener('click', () => {
+
+    aboutOverlay.style.display = 'none';
+
+});
 
 //
